f18f89f66ff1c7b251f668cd58e3c98121be6c9e
santiago <santiago.sanchez.ssaa@gmail.com>
2021-09-23 14:52:20 -0500

    client

--- client/hwclient.py
+++ client/hwclient.py
@@ -4,11 +4,13 @@ import sys, os
 import zmq
 
 
-# 50 mb -> 52428800 Bytes
-#CHUNK_SIZE = 1024*1024*1024 # 1gb
-CHUNK_SIZE = 50
+CHUNK_SIZE = 1024*1024*50
 ROUTE = "tcp://localhost:8001"
-CHUNK_SIZE = 1024*1024*5
+
+
+def get_file_hash(file):
+    sha1.update(file)
+    return sha1.hexdigest()
 
 class SocketRequest():
     """SocketRequest class"""
@@ -20,6 +22,7 @@ class SocketRequest():
         self.file_content : bytes = None
         self.file_chunks : int = None
         self.file_hash : str = None
+        self.multipart_message : list = [None, None]
         self.operation : str = kwargs.get("operation")
         self.sharelink : str = None
         self.username : str = kwargs.get("username")
@@ -36,11 +39,14 @@ class SocketRequest():
     def set_file_hash(self, file_hash: str) -> None:
         self.file_hash = file_hash    
 
+    def set_multipart_message(request: dict={}, content:bytes = None):
+        if request:
+            self.multipart_message[0] = json.dumps(request).encode("utf-8")
+        elif content:
+            self.multipart_message[1] = content
+
     def set_share_link(self, sharelink: str) -> None:
         self.sharelink = sharelink
-
-    def get_file_size(self) -> int:
-        return os.stat(self.filename).st_size
 
     def get_upload_message(self) -> dict:
         return {
@@ -48,8 +54,13 @@ class SocketRequest():
             "is_sending_message": self.is_sending_message,
             "filename": self.filename,
             "file_hash": self.file_hash,
-            "operation": self.operation
+            "operation": self.operation,
+            "username": self.username
         }
+
+    def get_download_message(self) -> dict:
+        return {}
+
 
 def create_request(*args) -> SocketRequest:
     request = SocketRequest(
@@ -64,59 +75,70 @@ def create_request(*args) -> SocketRequest:
         raise Exception("operation {} not allowed".format(request.operation))
     return request
 
-def get_file_hash(file):
-    sha1.update(file)
-    return sha1.hexdigest()
 
 def main(socket, *args) -> None:
     request : SocketRequest = create_request(*args)
-    multipart_message : list = [None, None]
 
     if request.operation == "upload":
         request.is_sending_message = True
-
-        file_chunks = request.get_file_size() / CHUNK_SIZE
-        if file_chunks != int(file_chunks):
-            file_chunks = int(file_chunks) + 1
 
         with open(request.filename, "rb") as file:
             chunk = file.read(CHUNK_SIZE)
             request.is_file_beginning = True
             request.set_file_hash(get_file_hash(file))
-            multipart_message[0] = json.dumps(request.get_upload_message()).encode("utf-8")
-            multipart_message[1] = b""
-            socket.send_multipart(multipart_message)
+            request.set_multipart_message(
+                request=request.get_upload_message(),
+                content=b""
+            )
+            socket.send_multipart(request.multipart_message)
             request.is_file_beginning = False
 
-            chunk_number : int = 1
             while chunk:
                 server_response : dict = json.loads(socket.recv_multipart()[0])
 
                 if server_response.get("message") == "ok":
-                    multipart_message[0] = json.dumps(request.get_upload_message()).encode("utf-8")
-                    multipart_message[1] = chunk
-                    socket.send_multipart(multipart_message)
-
-                chunk_number += 1
+                    request.set_multipart_message(
+                        request=request.get_upload_message(),
+                        content=chunk
+                    )
+                    socket.send_multipart(request.multipart_message)
                 chunk = file.read(CHUNK_SIZE)
 
         _ = socket.recv_multipart()
 
     else:
-        multipart_message[1] = b""
+        request.set_multipart_message(content=b"")
         if request.operation == "download":
             request.set_file_chunks(0)
 
-    multipart_message[1] = b""
-    socket.send_multipart(multipart_message)
+    request.set_multipart_message(content=b"")
+    socket.send_multipart(request.multipart_message)
     server_response : dict = json.loads(socket.recv_multipart()[0])
+
     if server_response.get("message") in ["message", "list"]:
         print(server_response.get("response"))
-    elif server_response.get("message") == "download": #TODO: THIS PART, AND THE SERVER
+    elif server_response.get("message") == "download": 
         path : str = "{}/{}".format(
             os.getcwd(),
             server_response.get("filename")
         )
+        with open(path,"wb") as file:
+            file.write(b"")
+
+        chunks = request.file_chunks
+        for i in range(chunks):
+            request.set_file_chunks(i)
+            request.set_multipart_message(
+                request=request.get_download_message(),
+                content=b""
+            )
+            socket.send_multipart(request.multipart_message)
+            socket_response = socket.recv_multipart()
+
+            with open(path, "ab") as file:
+                file.write(socket_response[1])
+        print("downloaded")
+
     else:
         raise Exception("operation error {}".format(server_response))
 
