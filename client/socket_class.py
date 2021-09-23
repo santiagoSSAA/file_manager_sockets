7a3978a9b43b0397b1ee3c310a7b8a8e72e414ef
santiago <santiago.sanchez.ssaa@gmail.com>
2021-09-23 14:58:17 -0500

    feat: separate class

---
+++ client/socket_class.py
@@ -0,0 +1,50 @@
+import json
+
+class SocketRequest():
+    """SocketRequest class"""
+
+    def __init__(self, **kwargs):
+        self.is_file_beginning : bool = False
+        self.is_sending_message : bool = False
+        self.filename : str = None
+        self.file_content : bytes = None
+        self.file_chunks : int = None
+        self.file_hash : str = None
+        self.multipart_message : list = [None, None]
+        self.operation : str = kwargs.get("operation")
+        self.sharelink : str = None
+        self.username : str = kwargs.get("username")
+
+    def set_filename(self, filename:str) -> None:
+        self.filename = filename
+
+    def set_file_content(self, file_content: bytes) -> None:
+        self.file_content = file_content
+
+    def set_file_chunks(self, file_chunks: int) -> None:
+        self.file_chunks = file_chunks
+
+    def set_file_hash(self, file_hash: str) -> None:
+        self.file_hash = file_hash    
+
+    def set_multipart_message(request: dict={}, content:bytes = None):
+        if request:
+            self.multipart_message[0] = json.dumps(request).encode("utf-8")
+        elif content:
+            self.multipart_message[1] = content
+
+    def set_share_link(self, sharelink: str) -> None:
+        self.sharelink = sharelink
+
+    def get_upload_message(self) -> dict:
+        return {
+            "is_file_beginning": self.is_file_beginning,
+            "is_sending_message": self.is_sending_message,
+            "filename": self.filename,
+            "file_hash": self.file_hash,
+            "operation": self.operation,
+            "username": self.username
+        }
+
+    def get_download_message(self) -> dict:
+        return {}
